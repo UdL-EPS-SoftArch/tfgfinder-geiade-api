f206eb440baf82c93cf2f488792e8bed67436e74
Jordi <jordiarenasr@gmail.com>
2025-02-18 21:39:21 +0100

    Interest class defined

--- src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Interest.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Interest.java
@@ -10,10 +10,49 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.ZonedDateTime;
 import java.util.Collection;
+
+@Entity
+@Table(name = "Interest") 
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Interest extends UriEntity<String> {
+
+	@Id
+	private String id;
+
+	@DateTimeFormat
+	private ZonedDateTime when;
+
+	public enum Status {
+		PENDING,
+		ACCEPTED,
+		REJECTED
+	}
+
+	@Enumerated (EnumType.STRING)
+	private Status status;
+
+	
+	public String getInvite() { return id; }
+
+	public void setInvite(String invite) { this.id = invite; }
+
+
+	public ZonedDateTime getWhen() { return when; }
+
+	public void setWhen(ZonedDateTime when) { this.when = when; }
+
+
+	public Status getStatus() { return status; }
+
+	public void setStatus(Status status) { this.status = status; }
+}
