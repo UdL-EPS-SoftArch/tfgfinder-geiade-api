9227d8ef06d4f7acdb1b15399631e2d7ef8fdf60
WhiteLion03 <sergi.baron.pascual@gmail.com>
2025-02-19 17:56:38 +0100

    Delete unnecessary imports

--- src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Agree.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Agree.java
@@ -1,21 +1,10 @@
 package cat.udl.eps.softarch.tfgfinder.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.ZonedDateTime;
 import java.util.Collection;
@@ -41,18 +30,16 @@ public class Agree extends UriEntity<String> {
     @Enumerated(EnumType.STRING)
     private Status status;
 
-
     public String getId() { return id; }
 
     public void setAgree(String invite) { this.id = invite; }
-
 
     public ZonedDateTime getWhen() { return when; }
 
     public void setWhen(ZonedDateTime when) { this.when = when; }
 
-
     public Status getStatus() { return status; }
 
     public void setStatus(Status status) { this.status = status; }
+
 }
