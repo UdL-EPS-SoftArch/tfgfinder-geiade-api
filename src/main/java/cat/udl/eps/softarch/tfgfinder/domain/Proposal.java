915f8149287d95824c26c9206a5979553d81ecec
BielGuallar <bielguallar@gmail.com>
2025-02-26 18:25:14 +0100

    Relations directedBy (director, external and professor) implemented

--- src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Proposal.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Proposal.java
@@ -1,6 +1,8 @@
 package cat.udl.eps.softarch.tfgfinder.domain;
 
 import java.util.Collection;
+import java.util.List;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -45,5 +47,30 @@ public class Proposal extends UriEntity<Long> {
 
     @ManyToOne
     private User user; // Reference to the User entity
+
+    // Relationship with Directors (0 to 2)
+    @ManyToMany
+    @JoinTable(
+            name = "proposal_directors",
+            joinColumns = @JoinColumn(name = "proposal_id"),
+            inverseJoinColumns = @JoinColumn(name = "director_id")
+    )
+    @Size(max = 2, message = "A proposal can have at most 2 directors")
+    private List<Director> directedByDirectors;
+
+    // Relationship with Professors (0 to 2)
+    @ManyToMany
+    @JoinTable(
+            name = "proposal_professors",
+            joinColumns = @JoinColumn(name = "proposal_id"),
+            inverseJoinColumns = @JoinColumn(name = "professor_id")
+    )
+    @Size(max = 2, message = "A proposal can have at most 2 professors")
+    private List<Professor> directedByProfessors;
+
+    // Relationship with External (0 to 1)
+    @ManyToOne
+    @JoinColumn(name = "external_id")
+    private External directedByExternal;
 
 }
