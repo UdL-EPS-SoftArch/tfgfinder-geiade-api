f53478c7ce8d29f447815dfe8bbcf452818d1c18
Diego Vázquez Teba <d.vazquezteba@gmail.com>
2025-03-18 15:14:32 +0100

    Revised the question: "Do you think we need a list? Revise it :)"
    So changed for a Chat

--- src/main/java/cat/udl/eps/softarch/tfgfinder/repository/ChatRepository.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/ChatRepository.java
@@ -5,13 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
 
 
 @RepositoryRestResource
 public interface ChatRepository extends CrudRepository<Chat, Long>, PagingAndSortingRepository<Chat, Long> {
 
-    List<Chat> findByProposalId(Long proposal_id);
+    Chat findByProposalId(Long proposal_id);
 
 
 }
