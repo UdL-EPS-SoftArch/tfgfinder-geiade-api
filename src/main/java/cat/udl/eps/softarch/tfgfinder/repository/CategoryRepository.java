c2bfe439389a1b092b9906d0722a15101611ead3
Isaac Torres Espuña <isaac@ite2002.net>
2025-03-26 18:40:36 +0100

    Create Category repository to make tests

---
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package cat.udl.eps.softarch.tfgfinder.repository;
+
+import cat.udl.eps.softarch.tfgfinder.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CategoryRepository extends CrudRepository<Category, Long>, PagingAndSortingRepository<Category, Long> {
+
+}
