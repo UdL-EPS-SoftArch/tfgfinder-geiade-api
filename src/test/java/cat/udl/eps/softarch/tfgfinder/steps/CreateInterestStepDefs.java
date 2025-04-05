0fae8f47d8db080a163017dc0ef0ca5ca529ec2f
Jordi <jordiarenasr@gmail.com>
2025-04-06 00:31:07 +0200

    dontExistInterestWithUserAndId

--- src/test/java/cat/udl/eps/softarch/tfgfinder/steps/CreateInterestStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/tfgfinder/steps/CreateInterestStepDefs.java
@@ -1,9 +1,13 @@
 package cat.udl.eps.softarch.tfgfinder.steps;
 
+import java.util.Optional;
+
 import cat.udl.eps.softarch.tfgfinder.domain.User;
 import cat.udl.eps.softarch.tfgfinder.repository.UserRepository;
 import cat.udl.eps.softarch.tfgfinder.domain.Proposal;
 import cat.udl.eps.softarch.tfgfinder.repository.ProposalRepository;
+import cat.udl.eps.softarch.tfgfinder.domain.Interest;
+import cat.udl.eps.softarch.tfgfinder.repository.InterestRepository;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -16,6 +20,11 @@ public class CreateInterestStepDefs {
     @Autowired
     private ProposalRepository proposalRepository;
 
+
+    @Autowired 
+    InterestRepository interestRepository;
+
+
     @Given("^There is a proposal with id \"([^\"]*)\"$")
     public void thereIsAProposalWithId(long id) 
     {
@@ -24,6 +33,22 @@ public class CreateInterestStepDefs {
             Proposal proposal = new Proposal();
             proposal.setId(id);
             proposalRepository.save(proposal);
+        }
+    }
+
+
+    @Given("^Don't exist Interest with user \"([^\"]*)\" and id \"([^\"]*)\"$")
+    public void DontExistInterestWithUserAndId(String user, long id)
+    {
+        Optional<Interest> optionalInterest = interestRepository.findById(id);
+
+        if (optionalInterest.isPresent()) {
+            
+            Interest interest = optionalInterest.get();
+
+            if (interest.getUser().getUsername().equals(user)) {
+                interestRepository.delete(interest);
+            }
         }
     }
 
