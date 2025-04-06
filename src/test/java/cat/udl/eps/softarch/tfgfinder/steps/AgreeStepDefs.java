0cc46de8beba4835056f864fe675b06d7924a173
Alex Gonzalez Tore <36084550+masteralex23@users.noreply.github.com>
2025-04-06 17:12:52 +0200

    updates
    It was not passing the tests, so it is updated. Pending to fix the response, as it gives 200 instead of 201

--- src/test/java/cat/udl/eps/softarch/tfgfinder/steps/AgreeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/tfgfinder/steps/AgreeStepDefs.java
@@ -1,25 +1,40 @@
 package cat.udl.eps.softarch.tfgfinder.steps;
 
 import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import java.nio.charset.StandardCharsets;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+
 
 import cat.udl.eps.softarch.tfgfinder.domain.Agree;
 import cat.udl.eps.softarch.tfgfinder.domain.Proposal;
 import cat.udl.eps.softarch.tfgfinder.domain.Student;
+import cat.udl.eps.softarch.tfgfinder.domain.User;
 import cat.udl.eps.softarch.tfgfinder.domain.Agree.Status;
 import cat.udl.eps.softarch.tfgfinder.repository.AgreeRepository;
 import cat.udl.eps.softarch.tfgfinder.repository.ProposalRepository;
 import cat.udl.eps.softarch.tfgfinder.repository.StudentRepository;
+import cat.udl.eps.softarch.tfgfinder.repository.UserRepository;
 
 public class AgreeStepDefs {
 
@@ -30,44 +45,101 @@ public class AgreeStepDefs {
     private AgreeRepository agreeRepository;
 
     @Autowired
-    private StudentRepository studentRepository;
+    private ProposalRepository proposalRepository;
 
     @Autowired
-    private ProposalRepository proposalRepository;
+    private UserRepository userRepository;
     
-    private Student student;
-    private Proposal proposal;
+    public static String newResourceUri;
 
-    @Given("^There is an authenticated student with id \"([^\"]*)\"$")
-    public void thereIsAnAuthenticatedStudent(String studentId) {
-        student = studentRepository.findById(studentId).orElse(null);
-    }
-
-    @When("he agrees to a proposal with id \"([^\"]*)\"$")
-    public void agreeToAProposal(Long proposalId) {
-        proposal = proposalRepository.findById(proposalId).orElse(null);
-    }
-
-    @Then("the agree with id \"([^\"]*)\" is created with status ACCEPTED$")
-    public void createAgree(Long agreeId) {
+    @When("There already is an agree with the following details:$")
+    public void There_already_is_and_agre_with_the_following_details(Map<String, String> agreeDetails) throws Throwable {
         Agree agree = new Agree();
-        agree.setStatus(Status.ACCEPTED);
-        agree.setUser(student);
+        Proposal proposal = new Proposal();
+        proposal.setTitle("Gestión Propuestas");
+        proposal.setDescription("Desarrollo de una aplicación web para gestionar propuestas académicas usando Spring Boot y Angular.");
+        proposal.setTiming("aaaaa");
+        proposal.setSpeciality("aaaaa");
+        proposal.setKind("aaaaa");
+        proposalRepository.save(proposal);
         agree.setProposal(proposal);
+            if(agreeDetails.get("date") != null){
+            ZonedDateTime date = ZonedDateTime.parse(agreeDetails.get("date"));
+            agree.setAgreeDate(date);
+        }
+        String statusString = agreeDetails.get("status");
+        Status status = Status.ACCEPTED;
+        if(statusString == "accepted"){
+            status = Status.ACCEPTED;
+        }else if(statusString == "pending"){
+            status = Status.PENDING;
+        }else if(statusString == "rejected"){
+            status = Status.REJECTED;
+        }
+        agree.setStatus(status);
+        if(agreeDetails.get("username") != null){
+            List<User> users = userRepository.findByIdContaining(agreeDetails.get("username"));
+            agree.setUser(users.get(0));
+        }
         agreeRepository.save(agree);
+        
     }
 
-    @Then("the student with id \"([^\"]*)\" and the proposal with id \"([^\"]*)\" are linked to the agree with id \"([^\"]*)\"$")
-    public void setStudentAndProposalOnAgree(String studentId, Long proposalId, Long agreeId) {
-        boolean exists = agreeRepository.existsById(agreeId);
-        assertEquals(exists, true);
-        Agree agree = agreeRepository.findById(agreeId).orElse(null);
-        assertEquals(agree.getProposal().getId(), proposalId);
-        assertEquals(agree.getUser().getId(), studentId);
+
+    @When("I agree to an agree with the following details:$")
+    public void createAgree(Map<String, String> agreeDetails) throws Exception{
+        
+        Proposal proposal = proposalRepository.findById(Long.parseLong(agreeDetails.get("proposalId"))).orElse(null);
+        if (proposal != null) {
+            List<Agree> existingAgrees = agreeRepository.findAgreeByProposal(proposal);
+            if(existingAgrees != null){
+                Agree existingAgree = existingAgrees.get(0);
+                
+                if(existingAgree == null){
+                    Agree agree = new Agree();
+                    agree.setStatus(Status.ACCEPTED);
+                    List<User> users = userRepository.findByIdContaining(agreeDetails.get("username"));
+                    agree.setUser(users.get(0));
+                    agree.setProposal(proposal);
+                    agree.setAgreeDate(ZonedDateTime.parse(agreeDetails.get("date")));
+        
+                    stepDefs.result = stepDefs.mockMvc.perform(
+                                    post("/agrees")
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .content(stepDefs.mapper.writeValueAsString(agree))
+                                            .characterEncoding(StandardCharsets.UTF_8)
+                                            .accept(MediaType.APPLICATION_JSON)
+                                            .with(AuthenticationStepDefs.authenticate()))
+                            .andDo(print());
+                    
+                }else{
+                    if(existingAgree.getUser() == null){
+                        existingAgree.setStatus(Status.ACCEPTED);
+                        existingAgree.setAgreeDate(ZonedDateTime.parse(agreeDetails.get("date")));
+                        List<User> users = userRepository.findByIdContaining(agreeDetails.get("username"));
+                        existingAgree.setUser(users.get(0));
+                        assertThat(existingAgree).isNotNull();
+                    }
+                }
+            }
+        }
+        
+        
+        
     }
 
-    @Then("the status is set to accepted")
-    public void the_status_is_set_to_accepted() {
-        // Write code here that turns the phrase above into concrete actions
+    @And("There is only (\\d+) agree with the details:$")
+    public void thereIsOnlyAgreeWithTheDeatils(int agreeCreatedNum, Map<String, String> agreeDetails) {
+        assertEquals(agreeCreatedNum, agreeRepository.count());
+        if(agreeCreatedNum == 1){
+            Agree agree = agreeRepository.findAll().iterator().next();
+            assertEquals(agree.getStatus(), Status.ACCEPTED);
+            Long proposalId = Long.parseLong(agreeDetails.get("proposalId"));
+            Assert.assertEquals(agree.getProposal().getId(), proposalId);
+            assertEquals(agree.getUser().getUsername(), agreeDetails.get("username"));
+            ZonedDateTime date = ZonedDateTime.parse(agreeDetails.get("date"));
+            assertEquals(agree.getAgreeDate(), date);
+        }
+        
     }
 }
--- src/test/resources/features/AgreeProposal.feature
+++ src/test/resources/features/AgreeProposal.feature
@@ -1,8 +1,43 @@
 Feature: Agree Proposal
   
-  Scenario: Agree a proposal as student
-    Given There is an authenticated student with id "studentID"
-    When he agrees to a proposal with id "proposalID"
-    Then the agree with id "agreeID" is created with status ACCEPTED
-    And the student with id "studentID" and the proposal with id "proposalID" are linked to the agree with id "agreeID"
+  Background:
+    Given There is no registered user with username "user"
+    And I'm not logged in
+    When I register a new user with username "user", email "user@sample.app" and password "password"
+    Then The response code is 201
+    And It has been created a user with username "user" and email "user@sample.app", the password is not returned
+    And I can login with username "user" and password "password"
+
+  Scenario: Agree to an already created agree
+    Given I can login with username "user" and password "password"
+    When There already is an agree with the following details:
+      | proposalId  | 1     |
+      | username    | user  |
+      | status      |pending|
+      | date        | 2024-03-17T08:00:00+01:00 |
+    And I agree to an agree with the following details:
+      | proposalId  | 1         |
+      | username    | user  |
+      | status      | accepted  |
+      | date        | 2024-03-17T08:00:00+01:00 |
+    Then The response code is 200
+    And There is only 1 agree with the details:
+      | proposalId  | 1     |
+      | username    | user  |
+      | status      | accepted  |
+      | date        | 2024-03-17T08:00:00+01:00 |
+
+Scenario: Agree to an already created agree
+    Given I can login with username "user" and password "password"
+    When I agree to an agree with the following details:
+      | proposalId  | 1     |
+      | username    | user  |
+      | status      | accepted  |
+      | date        | 2024-03-17T08:00:00+01:00 |
+    Then The response code is 200
+    And There is only 1 agree with the details:
+      | proposalId  | 1     |
+      | username    | user  |
+      | status      | accepted  |
+      | date        | 2024-03-17T08:00:00+01:00 |
 
