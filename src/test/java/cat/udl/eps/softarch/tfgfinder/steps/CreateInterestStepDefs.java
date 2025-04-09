9a57031deb37bf3d7a30d2424ae0323e4f49aa58
Jordi <jordiarenasr@gmail.com>
2025-04-09 17:43:55 +0200

    All scenario steps normalized

--- src/test/java/cat/udl/eps/softarch/tfgfinder/steps/CreateInterestStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/tfgfinder/steps/CreateInterestStepDefs.java
@@ -43,7 +43,8 @@ public class CreateInterestStepDefs {
     @Autowired
     private UserRepository userRepository;
 
-
+    // Ensure a proposal with the given ID exists in the repository. 
+    // If it doesn't, create a new one with dummy data.
     @Given("^There is a proposal with id \"([^\"]*)\"$")
     public void thereIsAProposalWithId(long id) 
     {
@@ -61,7 +62,8 @@ public class CreateInterestStepDefs {
         }
     }
 
-
+    // Ensure a user with the given username and password exists. 
+    // If not, create and save a new user with the given credentials.
     @And("^There is a user with user \"([^\"]*)\" and password \"([^\"]*)\"$")
     public void thereIsAUserWithUserAndPassword(String username, String password)
     {
@@ -80,7 +82,8 @@ public class CreateInterestStepDefs {
         }
     }
 
-
+    // Ensure that a user with the specified username does NOT exist. 
+    // If the user exists, delete them.
     @And("^There isn't a user with user \"([^\"]*)\"$")
     public void thereIsntAUserWithUserAndPassword(String username)
     {
@@ -90,7 +93,8 @@ public class CreateInterestStepDefs {
         }
     }
 
-
+    // Ensure there is no Interest with the given user and ID. 
+    // If it exists and belongs to that user, delete it.
     @Given("^Don't exist Interest with user \"([^\"]*)\" and id \"([^\"]*)\"$")
     public void DontExistInterestWithUserAndId(String user, long id)
     {
@@ -108,6 +112,9 @@ public class CreateInterestStepDefs {
         }
     }
 
+    // Verify the number of Interest entities for a user and proposal.
+    // If `num` is 0, assert that no Interest exists for that user and proposal.
+    // If `num` is greater than 0, assert that at least one Interest exists.
     @And("^There is ([^\"]*) Interest created with user \"([^\"]*)\" and proposal id \"([^\"]*)\"$")
     public void ThereIs1InterestCreatedWithUserAndProposalId(int num, String user, long proposalId)
     {
@@ -142,14 +149,15 @@ public class CreateInterestStepDefs {
         }
     }
 
-
+    // Creates an Interest using a given username and proposal ID.
     @When("I create an interest with user \"([^\"]*)\" and proposal id \"([^\"]*)\"")
     public void ICreateAnInterestWithUserAndProposalId(String username, long proposalId)
     {
-        
+
     }
 
-
+    // Sends a POST request to the API to express interest in a proposal 
+    // by the currently authenticated user.
     @Then("^I show interest to proposal id \"([^\"]*)\"$")
     public void IShowInterestToProposalId(long id) throws Exception
     {
--- src/test/resources/features/CreateInterest.feature
+++ src/test/resources/features/CreateInterest.feature
@@ -19,7 +19,7 @@ Scenario: Create an interest being logged in
 Scenario: Create an interest without being logged in
     Given Don't exist Interest with user "user" and id "1"
     And There isn't a user with user "user"
-    When I create an interest with user "user" and proposal id "1"
+    When I show interest to proposal id "1"
     Then The response code is 401
     And The error message is "Unauthorized"
     And There is 0 Interest created with user "user" and proposal id "1"
@@ -27,7 +27,7 @@ Scenario: Create an interest without being logged in
 
 Scenario: Create an interest that already exists
     Given Exists an interest with user "user" and proposal id "1"
-    When I create an interest with user "user" and proposal id "1"
+    When I show interest to proposal id "1"
     Then The response code is 401
     And The error message is "Unauthorized"
     And There is 1 Interest created with user "user" and proposal id "1"
