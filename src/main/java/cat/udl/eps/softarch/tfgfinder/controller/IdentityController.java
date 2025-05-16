a96c7dfd8876382ee1ff7bf9c844f2e7c3c0f2ce
BielGuallar <bielguallar@gmail.com>
2025-05-16 22:54:16 +0200

    Tests were not passing, getCurrentUser added to RestController

--- src/main/java/cat/udl/eps/softarch/tfgfinder/controller/IdentityController.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/controller/IdentityController.java
@@ -4,7 +4,9 @@ import cat.udl.eps.softarch.tfgfinder.domain.User;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.PersistentEntityResource;
 import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +22,9 @@ public class IdentityController {
     User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
     return resourceAssembler.toFullResource(user);
+  }
+  @GetMapping("/identity")
+  public User getCurrentUser(@AuthenticationPrincipal User user) {
+    return user;
   }
 }
